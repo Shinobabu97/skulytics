7dcd1845a5c9ba1c548ce5dbf8059ac070da80b9
Shinobabu97 <130092410+Shinobabu97@users.noreply.github.com>
2025-06-10 21:29:53 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -138,5 +138,7 @@ async def get_forecast():
     return forecast_data
 
 if __name__ == "__main__":
+    import os
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+
